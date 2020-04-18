0fe574e993cac2adacfaae10131f61cbd494c84f
Jo-Yewon <yeawonjo@gmail.com>
2020-04-18 23:25:34 +0900

    rt_task 생성

--- Generation.py
+++ Generation.py
@@ -7,6 +7,7 @@ class TaskGen:
         # 입력 정보
         self.n_cores = None
         self.n_tasks = None
+        self.wcet_min = self.wcet_max = None
         self.period = None
         self.mem_total = None
         self.util_cpu = None
@@ -17,6 +18,8 @@ class TaskGen:
         self.get_input()
 
     def gen_task(self):
+        self.n_tasks = round(self.util_cpu * self.n_cores / (((self.wcet_max + self.wcet_min) / 2) / self.period))
+
         util_cpu_1_task = self.util_cpu / self.n_tasks * self.n_cores
         mem_req_1_task = self.total_mem_usage / self.n_tasks
 
@@ -27,21 +30,23 @@ class TaskGen:
         #
         with open("input_rt_tasks.txt", "w", encoding='UTF8') as f:
             f.write("{}\n".format(self.n_tasks))
-            for i in range(self.n_tasks):
-                print("--------------------------")
-                print(f'Task Number: {i + 1}')
-                self.do_gen_task(f, util_cpu_1_task, mem_req_1_task)
-                print("--------------------------")
+            util = 0
+            while util < self.util_cpu * self.n_cores:
+                # print("--------------------------")
+                # print(f'Task Number: {i + 1}')
+                util += self.do_gen_task(f, util_cpu_1_task, mem_req_1_task)
+                # print("--------------------------")
+            print("util sum: {}".format(util))
 
         # print(f'util_total_mem: {format(self.get_util_overhead_by_mem(self.mem_req_total), ".6f")}')
         # print(f'util_total_cpu: {format(self.util_sum_cpu, ".6f")}')
         # print(f'mem_req_total: {format(self.mem_req_total, ".0f")}')
 
     def do_gen_task(self, input_file, util_cpu_1_task, mem_req_1_task):
-        # wcet = self.wcet_min + self.get_rand(self.wcet_max - self.wcet_min)
+        wcet = self.wcet_min + self.get_rand(self.wcet_max - self.wcet_min)
         # duration = wcet / util_cpu_1_task + int(self.get_rand(wcet / util_cpu_1_task / 2)) - int(self.get_rand(wcet / util_cpu_1_task / 2))
         duration = self.period
-        wcet = int(duration * util_cpu_1_task) + int(self.get_rand(duration * util_cpu_1_task)) - int(self.get_rand(duration * util_cpu_1_task))
+        # wcet = int(duration * util_cpu_1_task) + int(self.get_rand(duration * util_cpu_1_task)) - int(self.get_rand(duration * util_cpu_1_task))
         if wcet == 0:
             wcet = 1
         memreq = mem_req_1_task + int(self.get_rand(mem_req_1_task / 2)) - int(self.get_rand(mem_req_1_task / 2))
@@ -56,6 +61,7 @@ class TaskGen:
         # print(f'memreq_total: {format(self.mem_req_total, ".0f")}')
 
         input_file.write(line)
+        return wcet / duration
 
     def get_util_overhead_by_mem(self, mem_used):
         return mem_used / self.mem_total
@@ -65,7 +71,8 @@ class TaskGen:
         try:
             with open(input_file, "r", encoding='UTF8') as f:
                 self.n_cores = int(f.readline())
-                self.n_tasks = int(f.readline())
+                #self.n_tasks = int(f.readline())
+                self.wcet_min, self.wcet_max = tuple(map(int, f.readline().split()))
 
                 line = f.readline().split()
                 self.period, self.mem_total = tuple(map(int, line[:2]))
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from DFGA import *
 import test_out_csv
 
 # test_out_csv.init()
-# TaskGen().gen_task()
+TaskGen().gen_task()
 # non_rt_gen()
 # dfga_run()
-hrt_run()
+# hrt_run()
 
 
