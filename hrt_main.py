232c4bc95c2ce848506cd1f4838f1749b5df7615
Jo-Yewon <yeawonjo@gmail.com>
2020-05-24 23:03:31 +0900

    fix: result_bug_fix

--- hrt_main.py
+++ hrt_main.py
@@ -11,24 +11,19 @@ def get_df(input_file="input/df.txt"):
         return int(f.readline())
 
 def hrt_run():
-    # # Original
-    # RTTask.total_power = NonRTTask.total_power = 0
-    # sim_time, verbose, processor, memories = get_configuration()
-    # rt_tasks = get_rt_tasks()
-    # non_rt_tasks = get_non_rt_tasks()
-    # SystemOriginal(sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks).run()
-    #
-    # # DG(비실시간 담당 코어가 다수일 수 있음)
-    # df = get_df()
-    # for c in range(-df, df+1):
-    #     RTTask.total_power = NonRTTask.total_power = 0
-    #     sim_time, verbose, processor, memories = get_configuration()
-    #     rt_tasks = get_rt_tasks()
-    #     non_rt_tasks = get_non_rt_tasks()
-    #     print("\n{}".format(c))
-    #     SystemDG(sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks, c).run()
-
+    # Original
+    RTTask.total_power = NonRTTask.total_power = 0
     sim_time, verbose, processor, memories = get_configuration()
     rt_tasks = get_rt_tasks()
-    SystemEDF(sim_time, verbose, processor, memories, rt_tasks, None).run()
+    non_rt_tasks = get_non_rt_tasks()
+    SystemOriginal(sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks).run()
+
+    # DG(비실시간 담당 코어가 다수일 수 있음)
+    df = get_df()
+    RTTask.total_power = NonRTTask.total_power = 0
+    sim_time, verbose, processor, memories = get_configuration()
+    rt_tasks = get_rt_tasks()
+    non_rt_tasks = get_non_rt_tasks()
+    print("\n{}".format(1))
+    SystemDG(sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks, 1).run()
 
